8df8e004e41b5e4e75b074f23c98190af09b8adc
Татьяна Чеснокова <tatianaches49@gmail.com>
2023-04-19 13:56:55 +0500

    added library

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public bool PasswordCheck(string source)
+        {
+           if (source.Length > 4 && !Char.IsDigit(source[0]))
+            {
+                return true;
+            }
+ 
+         
+
+            return false;
+
+          
+        }
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           Class1 g = new Class1();
+            bool actual = g.PasswordCheck("nhjkjkj");
+            bool expected = true;
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
